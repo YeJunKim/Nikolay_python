52db2ca7905f60fd8ad15283b59dd86bdc1d3e84
KimYeJun <sillon2808@gmail.com>
2017-07-12 09:35:02 -0700

    update whois.py (input arg)

--- whois.py
+++ whois.py
@@ -1,19 +1,18 @@
 import pythonwhois
 import json
-import re
+import sys
 
-
-json_data = pythonwhois.net.get_whois_raw('www.google.com', with_server_list=False)
+json_data = pythonwhois.net.get_whois_raw(sys.argv[1], with_server_list=False)
 
 parse = pythonwhois.parse.parse_raw_whois(json_data, True)
 
-jsonarray = json.dumps(parse, ensure_ascii=True, indent=4, separators=None, encoding="utf-8", default=None, sort_keys=True)
-
+jsonarray = json.dumps(parse, ensure_ascii=True, indent=4, 
+        separators=None, encoding="utf-8", default=None, sort_keys=True)
 
 print jsonarray
 
 
-obj = open("data.json", 'wb')
+obj = open("whois_data.json", 'wb')
 obj.write(jsonarray)
 obj.close
 
