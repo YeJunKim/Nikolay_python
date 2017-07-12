92694210982fc23285ce451e8e783fac40a790f8
KimYeJun <sillon2808@gmail.com>
2017-07-12 10:53:09 -0700

    update whois.py (please input url www.google.kr).

--- whois.py
+++ whois.py
@@ -6,8 +6,7 @@ json_data = pythonwhois.net.get_whois_raw(sys.argv[1], with_server_list=False)
 
 parse = pythonwhois.parse.parse_raw_whois(json_data, True)
 
-jsonarray = json.dumps(parse, ensure_ascii=True, indent=4, 
-        separators=None, encoding="utf-8", default=None, sort_keys=True)
+jsonarray = json.dumps(parse, ensure_ascii=True, indent=4, separators=None, encoding="utf-8", default=False, sort_keys=True)
 
 print jsonarray
 
