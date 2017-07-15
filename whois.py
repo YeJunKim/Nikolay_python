cefca9e6dd6cf1c6d689dbac9a627bab0abc53b5
YeJunKim <sillon2808@gmail.com>
2017-07-15 05:53:33 -0700

    update whois.py

--- whois.py
+++ whois.py
@@ -2,16 +2,21 @@ import pythonwhois
 import json
 import sys
 
+
+
 json_data = pythonwhois.net.get_whois_raw(sys.argv[1], with_server_list=False)
 
-parse = pythonwhois.parse.parse_raw_whois(json_data, True)
+a = json.dumps(json_data[0], indent=4)
 
-a = json.dumps(parse, ensure_ascii=True, indent=4, encoding="utf-8", sort_keys=False)
+_data = a.split("\\n")
 
-print a
+data = json.dumps(_data, indent=4)
+
+print data
 
 
 obj = open("data_whois.json", 'wb')
-obj.write(a)
+obj.write(data)
 obj.close
+
 
